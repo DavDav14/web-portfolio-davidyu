4c75ce56749aa79e89af95918a84a9d7be60a980
DavDav <davidyuxe990@gmail.com>
2021-10-19 22:08:22 +0700

    Add Qualification in HTML,CSS,Js

--- js/main.js
+++ js/main.js
@@ -51,3 +51,23 @@ function toggleSkills(){
 skillsHeader.forEach((el) => {
     el.addEventListener('click',toggleSkills)
 })
+
+// =>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Qualification Tabs <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<=
+const tabs = document.querySelectorAll('[data-target]'),
+      tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab => {
+    tab.addEventListener('click',() =>{
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent =>{
+            tabContent.classList.remove('qualification_active')
+        })
+        target.classList.add('qualification_active')
+
+        tabs.forEach(tab =>{
+            tab.classList.remove('qualification_active')
+        })
+        tab.classList.add('qualification_active')
+    })
+})
