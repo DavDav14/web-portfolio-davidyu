b89eff25cc73af1f676e969732aa1f7f4b769c01
DavDav <davidyuxe990@gmail.com>
2021-10-19 23:17:30 +0700

    Add Services in HTML,CSS,Js

--- js/main.js
+++ js/main.js
@@ -71,3 +71,26 @@ tabs.forEach(tab => {
         tab.classList.add('qualification_active')
     })
 })
+
+// =>>>>>>>>>>>>>>>>>>>>>>>>>>>>> SERVICES MODAL <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<=
+const modalViews = document.querySelectorAll('.services_modal'),
+      modalBtns = document.querySelectorAll('.services_button'),
+      modalCloses = document.querySelectorAll('.services_modal-close')
+
+let modal = function(modalClick){
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((modalBtn,i) =>{
+    modalBtn.addEventListener('click',() => {
+        modal(i)
+    })
+})
+
+modalCloses.forEach((modalClose) => {
+    modalClose.addEventListener('click', () => {
+        modalViews.forEach((modalView) =>{
+            modalView.classList.remove('active-modal')
+        })
+    })
+})
