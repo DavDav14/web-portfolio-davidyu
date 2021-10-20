cb9eb46bf1b4a4b1a70e7cfbc5586307c811d156
DavDav <davidyuxe990@gmail.com>
2021-10-20 21:35:11 +0700

    Add ScrollUp Button in HTML,CSS,Js

--- js/main.js
+++ js/main.js
@@ -138,3 +138,11 @@ function scrollHeader(){
     if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
+
+/*==================== SHOW SCROLL UP ====================*/ 
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
