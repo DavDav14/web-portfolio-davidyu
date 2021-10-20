11fd650ddfee86c09dec7aa6099cb0f03a4a9434
DavDav <davidyuxe990@gmail.com>
2021-10-20 21:20:46 +0700

    Add Active Navbar in HTML,CSS,Js

--- js/main.js
+++ js/main.js
@@ -101,12 +101,32 @@ let swiper = new Swiper('.portfolio_container', {
     loop: true,
     
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
     },
     pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
+        el: '.swiper-pagination',
+        clickable: true,
     },
+    
+});
 
-  });
+// =>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Scroll Sections Active Link <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<=
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
